1c085dd4712669b3b99365344f436244b65789cb
Unknown <rispir45@gmail.com>
2018-07-10 15:35:08 -0400

    Set up initial header and files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h3>Ain't no party like an</h3>
+          <h1>API Party</h1>
+        </header>
+      </div>
+    )
+  }
+}
+export default App
